772fa7e1037c7a83f96e3c6b2ef70910b247cf51
Harry Bloch <harryfbloch@gmail.com>
2018-10-08 17:54:43 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,7 +23,11 @@ class Scraper
   end
   
   def get_courses
-    
+    doc.css(".post").each do |post|
+      course = Course.new 
+      course.title = post.css("h2").text 
+      course.schedule = post.css(".date").text 
+      course.description = post.css("p").text 
   end
   
   def make_courses
